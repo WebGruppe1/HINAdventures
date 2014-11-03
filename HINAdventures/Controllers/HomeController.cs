a4659a186f1895ff07c680f09fa47d0d2b4971e3
tmfredriksen <tord.fredriksen@gmail.com>
2014-11-03 10:59:37 +0100

    edit firstpage

--- HINAdventures/Controllers/HomeController.cs
+++ HINAdventures/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace HINAdventures.Controllers
             return View();
         }
 
+        public ActionResult FirstPage()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
