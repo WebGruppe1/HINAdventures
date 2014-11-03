8484c42821d2c4e2a939cb9535841c5d9485f6b0
TommyLanghelle <tomlanghe@gmail.com>
2014-11-03 11:14:31 +0100

    Added gamewindow view

--- HINAdventures/Controllers/HomeController.cs
+++ HINAdventures/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace HINAdventures.Controllers
             return View();
         }
 
+        public ActionResult Game()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
