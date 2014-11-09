5fab445c589b3685294f259b4ee8f614e1f5739d
Dag Ivarsoy <dagivarsoy@gmail.com>
2014-11-09 11:36:46 +0000

    Added Commandhub to Game page

---
+++ HINAdventures/Hubs/CommandHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+
+namespace HINAdventures.Hubs
+{
+    [HubName("Command")]
+    public class CommandHub : Hub
+    {
+        public void Command(string command)
+        {
+            string message = command + "\n";
+//            string message = "Command does not exist";
+            Clients.Caller.CommandResponse(message);
+        }
+    }
+}
