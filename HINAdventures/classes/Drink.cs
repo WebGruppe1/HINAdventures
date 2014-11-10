384f01f2396eae8b70c77d95293e8474c309b64e
tmfredriksen <tord.fredriksen@gmail.com>
2014-11-10 14:44:03 +0100

    drink and open

---
+++ HINAdventures/classes/Drink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HINAdventures.classes
+{
+    public class Drink
+    {    
+        public static string DrinkCommand(string item)
+        {
+            return "You drank " + item;
+        }
+    }
+}
