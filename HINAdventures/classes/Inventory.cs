c5155537d95af87d84d2b7e2d695db1109143d38
tmfredriksen <tord.fredriksen@gmail.com>
2014-11-10 15:05:23 +0100

    inventory

---
+++ HINAdventures/classes/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HINAdventures.classes
+{
+    public class Inventory
+    {
+
+        // return Item.getItemsList();
+
+        /*
+         * I en klasse Item:
+         * 
+         * public List<Item> getItems List = repository.getItemsByUserID();...
+         */
+    }
+}
