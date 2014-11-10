08a33c435a76e514b7bbf304cc18b55e99b76f30
tmfredriksen <tord.fredriksen@gmail.com>
2014-11-10 14:04:57 +0100

    drink and open command

---
+++ HINAdventures/classes/Open.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HINAdventures.classes
+{
+    public class Open
+    {
+        public static string OpenCommand(String item)
+        {
+            return "You opened " + item;
+        }
+    }
+}
