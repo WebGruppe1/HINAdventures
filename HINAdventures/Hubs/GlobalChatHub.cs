f53c09fed4fb66a6ce653379ce31a3af1b60e529
KristianAlm <kristian.alm83@gmail.com>
2014-11-10 17:13:56 +0100

    Litt kommentering

--- HINAdventures/Hubs/GlobalChatHub.cs
+++ HINAdventures/Hubs/GlobalChatHub.cs
@@ -8,37 +8,49 @@ using System.Threading.Tasks;
 
 namespace HINAdventures.Hubs
 {
+    /// <summary>
+    /// This takes care of the chat function between players in the game
+    /// Kristian Alm 10.11.2014
+    /// </summary>
     public class GlobalChatHub : Hub
     {
+        //List of all online players
         static ConcurrentDictionary<string, string> userList = new ConcurrentDictionary<string, string>();
 
+        //Send the name with the message to all players connected
         public void Send(string name, string message)
         {
             Clients.All.addNewMessageToPage(name, message);
         }
 
+        //Add the new player to the list of players and send the information about the new player to all others.
         public void Join(string userName, string id)
         {
+            //Does nothing if player is already in the list
             if (userList.ContainsKey(userName))
             {
 
             }
-
+            
+            //The player entering the chatroom gets a list of all players online
             else
             {
                 userList.TryAdd(userName, id);
                 foreach (KeyValuePair<String, String> entry in userList)
                     Clients.Caller.showConnected(entry.Key);
             }
+            //Update all other with information about the new player
             Clients.Others.showConnected(userName);
         }
 
+        //Private chat between two players, sends message to specific player
         public void SendToSpecific(string name, string message, string to)
         {
             Clients.Caller.addNewMessageToPage(name, message);
             Clients.Client(userList[to]).addNewMessageToPage(name, message);
         }
 
+        //On disconnect from signalR it removes you from the list and sends information to a function in GlobalChat.cshtml
         public override Task OnDisconnected(bool stopCalled)
         {
             var name = userList.FirstOrDefault(x => x.Value == Context.ConnectionId.ToString());
