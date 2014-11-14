e8cdf9bbc9d1bc8412348c165f489e3a64daf5b4
TommyLanghelle <tomlanghe@gmail.com>
2014-11-14 11:36:12 +0100

    Added repository

---
+++ HINAdventures/classes/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HINAdventures.classes
+{
+    public interface IRepository
+    {
+        public List<String> getAvailableRooms();
+
+    }
+}
---
+++ HINAdventures/classes/Repository.cs
@@ -0,0 +1,28 @@
+﻿using HINAdventures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HINAdventures.classes
+{
+    public class Repository : IRepository
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public List<String> getAvailableRooms()
+        {
+            List<String> list = new List<String>();
+            
+            var rooms = from a in db.Rooms select a;
+
+            foreach (Room r in rooms)
+            {
+                list.Add(r.Name);
+            }
+            
+            return list;
+        }
+
+    }
+}
