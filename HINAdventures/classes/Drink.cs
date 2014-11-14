5efc8c25b3a3741b1b1bf3f9ec62c7abaf82b10e
eivindjs <eivind.skreddernes@gmail.com>
2014-11-14 14:10:50 +0100

    oppdatert kommandoer og lagt til throw

--- HINAdventures/classes/Drink.cs
+++ HINAdventures/classes/Drink.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HINAdventures.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,9 +8,28 @@ namespace HINAdventures.classes
 {
     public class Drink : ICommand
     {    
+        private IRepository repos;
+        private List<Item> items;
+        public Drink()
+        {
+            repos = new Repository();
+            items = repos.GetAllItems();
+        }
         public string RunCommand(string item)
         {
-            return "You drank " + item;
+            if (items != null)
+            {
+                for (int i = 0; i < items.Count; i++)
+                {
+                    Item it = items[i];
+                    if (it.isDrinkable)
+                        item = "You drank " + item;
+                    else
+                        item = "You can't drink this";
+                }
+            }
+            return item;
         }
+    
     }
 }
