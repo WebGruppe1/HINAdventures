2fa8bf6caf0c0cd8c6e603715421eb33b9bbc6f0
eivindjs <eivind.skreddernes@gmail.com>
2014-11-14 10:48:23 +0100

    Lagt til mer på hit kommando

--- HINAdventures/classes/Hit.cs
+++ HINAdventures/classes/Hit.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HINAdventures.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,9 +11,31 @@ namespace HINAdventures.classes
 
         public static string HitCommand(string item)
         {
-            string hit = "You just struck a " + item;
+                  string hit = "";
+                  List<Item> items = GetAlleItems();
+                  for (int i = 0; i < items.Count; i++)
+                  {
+                      Item it = items[i];
+                      if (item == it.Name.ToLower() || item == it.Name)
+                      {
+                          hit = "You just struck a " + item;
+                      }
+                      else
+                      {
+                          hit = "The Item you are trying to hit/struck does not exist";
+                      }
+                  }
+                  return hit;
+           
+        }
+        static public List<Item> GetAlleItems()
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var itemListe = context.Items.ToList();
+                return itemListe;
 
-            return hit;
+            }
         }
     }
 }
