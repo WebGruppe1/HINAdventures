8906fa35f6916d452826c4fae5eaa73f819e3711
eivindjs <eivind.skreddernes@gmail.com>
2014-11-20 12:52:00 +0100

    Lagt til give og kill

---
+++ HINAdventures/classes/Give.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HINAdventures.classes
+{
+    public class Give : ICommandTwoArgs
+    {
+        public string RunCommand(string argument, string id)
+        {
+            return "";
+        }
+    }
+}
---
+++ HINAdventures/classes/Kill.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HINAdventures.classes
+{
+    public class Kill
+    {
+    }
+}
