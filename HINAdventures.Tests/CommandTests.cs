f84797116937d9a646d80c443a5207bfc5f51fe6
Dag Ivarsoy <dagivarsoy@gmail.com>
2014-11-21 13:49:34 +0000

    Wrote test for help class

---
+++ HINAdventures.Tests/CommandTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HINAdventures.classes;
+using HINAdventures.Models;
+
+namespace HINAdventures.Tests
+{
+    [TestClass]
+    public class CommandTests
+    {
+        [TestMethod]
+        public void TestHelp()
+        {
+            //Arrange
+            var help = new Help();
+            string expected = "List of available commands:";
+            //Act
+            String result = help.RunCommand();
+            //Assess
+            Assert.AreEqual(expected, result.Substring(0, 27));
+
+        }
+    }
+}
