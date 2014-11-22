539ec57ab1cb97c506f309391352fbc139e4a850
tmfredriksen <tord.fredriksen@gmail.com>
2014-11-22 20:32:39 +0100

    tests

---
+++ HINAdventures.Tests/Controllers/AuthenticationTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HINAdventures.classes;
+using HINAdventures.Controllers;
+using Moq;
+using System.Web.Mvc;
+using HINAdventures.Models;
+/**
+ * AuthenticationTest.cs
+ * 
+ * The goal with this unit test is testing user authentication.
+ * 
+ * T.Fredriksen 21.11.14
+ **/
+namespace HINAdventures.Tests.Controllers
+{
+    [TestClass]
+    public class AuthenticationTest
+    {
+        private Mock<IRepository> repos;
+        public HomeController CreateControllerForUser(string userName)
+        {
+            var mock = new Mock<ControllerContext>();
+            mock.SetupGet(p => p.HttpContext.User.Identity.Name).Returns(userName);
+            mock.SetupGet(p => p.HttpContext.Request.IsAuthenticated).Returns(true);
+            var controller = new HomeController();
+            controller.ControllerContext = mock.Object;
+            return controller;
+        }
+
+        [TestInitialize]
+        public void setupindex()
+        {
+            repos = new Mock<IRepository>();
+        }
+        [TestMethod]
+        public void TestIndexMethod()
+        {
+
+
+            var controller = CreateControllerForUser("testuser@example.com");
+
+            var result = controller.User as ViewResult;
+
+
+            //  Assert.IsInstanceOfType(result.ViewData.Model, typeof(ApplicationUser));
+
+        }
+    }
+}
---
+++ HINAdventures.Tests/Controllers/RedirectionTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using HINAdventures.Models;
+using Moq;
+
+namespace HINAdventures.Tests.Controllers
+{
+    [TestClass]
+    public class RedirectionTest
+    {
+        /*
+        [TestMethod]
+        public async Task Register_RegisterValidUser_EnsureRedirectToIndexHome()
+        {
+            // Arrange
+            var userManagerStub = new Mock<ApplicationUserManager(ApplicationUser, null)();
+            var tcs = new TaskCompletionSource<IdentityResult>();
+            tcs.SetResult(new IdentityResult(true));
+            userManagerStub.Setup(s => s.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).Returns(tcs.Task);
+
+            var signInManagerStub = new Mock<ISignInManager>();
+            signInManagerStub.Setup(s => s.SignInAsync(It.IsAny<ApplicationUser>(), It.IsAny<bool>())).Returns(Task.FromResult(true));
+
+            var sut = new AccountController(userManagerStub.Object) { SignInManager = signInManagerStub.Object };
+
+            // Act
+            var result = await sut.Register(new RegisterViewModel() { Password = "fakePw" }) as RedirectToRouteResult;
+
+            // Assert
+            Assert.AreEqual<string>("Index", result.RouteValues["action"].ToString());
+            Assert.AreEqual<string>("Home", result.RouteValues["controller"].ToString());
+        } */
+    }
+}
