852ccec17768d0e452cd7a52fec393633e048bff
TommyLanghelle <tomlanghe@gmail.com>
2014-11-24 12:56:58 +0100

    scout ser users og items i samme rom. ikke bugfritt enda

--- HINAdventures/classes/Scout.cs
+++ HINAdventures/classes/Scout.cs
@@ -16,12 +16,54 @@ namespace HINAdventures.classes
         public String RunCommand(String userID)
         {
             List<Room> rooms =  repo.getAvailableRooms(userID);
-            String returnString = "Nearby rooms: ";
+            List<Item> items = repo.GetAllItems();
+            List<ApplicationUser> users = repo.GetAllUsers();
+            ApplicationUser user = repo.GetUser(userID);
+
+            String returnString = "You look around and see doors labeled with the names ";
 
             foreach(Room room in rooms)
             {
-                returnString += room.Name + ", ";
+                if(room.Equals(rooms.Last()))
+                    returnString += room.Name + ". ";
+                else
+                    returnString += room.Name + ", ";
             }
+
+            if(items.Count > 0)
+            {
+                returnString += "When you look around you see some items: ";
+                foreach(Item item in items)
+                {
+                    if (item.ApplicationUser == null && item.Room == user.Room)
+                    {
+                        if (item.Equals(items.Last()))
+                            returnString += item.Name + ". ";
+                        else
+                            returnString += item.Name + ", ";
+                    }
+                }
+            }
+
+            if(users.Count > 0)
+            {
+                string availableUsers = "";
+                foreach(ApplicationUser u in users)
+                {
+                    if (u.Room == user.Room)
+                    {
+                        if (u.Equals(users.Last()))
+                            availableUsers += u.FirstName;
+                        else
+                            availableUsers += u.FirstName + ", ";
+                    }
+
+                }
+                if (availableUsers != "")
+                    returnString += "It looks like you're not alone in this room! you also see " + availableUsers + " standing around doing nothing. ";
+            }
+
+
             return returnString;
         }
     }
