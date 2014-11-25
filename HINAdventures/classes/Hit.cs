ee9fc6bde6d069819c3fab6c4fa82926978a3874
eivindjs <eivind.skreddernes@gmail.com>
2014-11-25 14:19:49 +0100

    fikset bug i hit

--- HINAdventures/classes/Hit.cs
+++ HINAdventures/classes/Hit.cs
@@ -19,30 +19,21 @@ namespace HINAdventures.classes
         }
         public string RunCommand(string item, string userID)
         {
-                  string hit = "";
-                  List<Item> items = repos.GetAllItems();
-                  for (int i = 0; i < items.Count; i++)
-                  {
-                      Item it = items[i];
-                      ApplicationUser user = repos.GetUser(userID);
-                      if (user.Room.Id == it.Room.Id)
-                      {
-                          if (item == it.Name.ToLower() || item == it.Name)
-                          {
-                              hit = "You just struck a " + it.Name;
-                          }
-                          else
-                          {
-                              hit = "The Item you are trying to hit/struck does not exist in this room";
-                          }
-                      }
-                      else
-                      {
-                          hit = "The Item you are trying to hit/struck does not exist in this room";
-                      }
-                  }
-                  return hit;
-           
+            List<Item> items = repos.GetAllItems();
+            for (int i = 0; i < items.Count; i++)
+            {
+                Item it = items[i];
+                ApplicationUser user = repos.GetUser(userID);
+                if (user.Room.Id == it.Room.Id)
+                {
+                    if (item == it.Name.ToLower() || item == it.Name)
+                    {
+                        return "You just struck a " + it.Name;
+                    }
+                }
+            }
+            return "The Item you are trying to hit/struck does not exist in this room";
+
         }
     }
 }
