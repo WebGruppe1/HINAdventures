63951eb682b3856bc6daf444aac09d57172941b3
Frederik <fredrik_johnsen92@hotmail.com>
2014-11-26 13:10:53 +0100

    lagt til whiteboard

--- HINAdventures/classes/Repository.cs
+++ HINAdventures/classes/Repository.cs
@@ -261,7 +261,10 @@ namespace HINAdventures.classes
             return user;
         }
 
-
-
+        public List<WhiteBoardBlog> GetAllWhiteBoards()
+        {
+            var whiteBoards = db.WhiteBoardBlogs.ToList();
+            return whiteBoards;
+        }
     }
 }
